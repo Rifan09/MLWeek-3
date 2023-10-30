3be13fee9dcd711da434e6bc4c6845812acaf4c9
Muhammad Rifani <134392217+Rifan09@users.noreply.github.com>
2023-10-30 09:59:22 +0800

    Menambahkan code baru Git operation

--- example.py
+++ example.py
@@ -1,3 +1,8 @@
 def git_opeation():
  print("I am adding example.py file to the remote repository.")
 git_opeation()
+
+# menambahkan code 
+def git_operation():
+  print(" rigth now 'm learn github by myself")
+git_operation()
